7d7829bbd23f3bd95eabe89e78a4e6249b4157d3
unknown <shafferjarrod99@aol.com>
2022-07-26 12:03:46 -0700

    current week homework

---
+++ answers.md
@@ -0,0 +1,9 @@
+// Git and Github Q&A
+
+#1 - git init
+#2 - git commit -m
+#3 - git remote add origin
+#4 -  git push -u origin main
+
+// HTML and CSS Videos
+#1
---
+++ loops.js
@@ -0,0 +1,7 @@
+for (var i = 0; i < 9; i++) {
+    if (i === 1) console.log(i + " for the treble");
+    else if (i === 2) console.log(i + " for the bass");
+    else if (i === 3) console.log(i + " for the fellas");
+    else if (i === 4) console.log(i + " for the plaid");
+    else if (i === 5) console.log
+  }
